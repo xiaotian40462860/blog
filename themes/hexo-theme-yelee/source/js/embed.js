527301fd79cff61d8ad1249aff1be9e56012a55c
xiaomo <83387856@qq.com>
2016-12-01 15:02:59 +0800

    修改图片地址

--- themes/hexo-theme-yelee/source/js/embed.js
+++ themes/hexo-theme-yelee/source/js/embed.js
@@ -640,7 +640,8 @@ function os(e) {
             return e.url
         },
         avatarUrl: function(e, t) {
-            return e.avatar_url || nt.data.default_avatar_url
+            return 'https://images.xiaomo.info/duoshuo/embed.php?s='+e.avatar_url
+                || 'https://images.xiaomo.info/duoshuo/embed.php?s='+nt.data.default_avatar_url
         },
         userAnchor: function(e) {npm
             var t = J.userUrl(e);
